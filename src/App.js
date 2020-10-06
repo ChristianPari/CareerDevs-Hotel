2da75ce2ff728c930afb55e5d16a732a9e1560d2
Christian Pari <ChristianPari@outlook.com>
2020-10-05 21:04:08 -0400

    Added context provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+// contexts
+import {RoomsContextProvider} from './Contexts/RoomContext'
+
+// components
+import NavBar from './Components/NavBar'
+import Body from './Components/Body'
+
 export default function App() {
   return (
     <div className="App">
-      The start of a reaction!
+      <RoomsContextProvider>
+        <NavBar />
+        <Body />
+      </RoomsContextProvider>
     </div>
   )
 }
