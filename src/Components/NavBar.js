11398d4027910f4819d91b13a20180c0411ede57
Christian Pari <ChristianPari@outlook.com>
2020-10-06 21:05:34 -0400

    Simply created the action btns for rendering the Rent or Return components via onClick

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,20 +5,31 @@ import Text from './Text'
 import Button from './Button'
 
 // contexts
+import { useActionUpdate } from '../Contexts/ActionContext'
 
 export default function NavBar() {
+  const actionUpdate = useActionUpdate()
+
   return (
     <div>
       <Text 
         tag='h1'
         text='CareerDevs Hotel'
       />
-      <Button 
-        label='Rent a Room'
-      />
-      <Button 
-        label='Return a Room'
-      />
+      <div className='action-btns'>
+        <Button 
+          label='Rent a Room'
+          onClick={() => {
+            actionUpdate('rent')
+          }}
+        />
+        <Button 
+          label='Return a Room'
+          onClick={() => {
+            actionUpdate('return')
+          }}
+        />
+      </div>
     </div>
   )
 }
