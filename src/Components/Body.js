d0a4c2a3d1958da83e128390fbd5b51df80ba5f8
Christian Pari <ChristianPari@outlook.com>
2020-10-06 21:04:13 -0400

    Imported the action context which is used in a switch to help render the correct component (rent/return)

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
 // components
+import Text from './Text'
+import Rent from './Rent'
+import Return from './Return'
 
 // contexts
+import { useAction } from '../Contexts/ActionContext'
 
 export default function Body() {
-  return (
-    <div>Body</div>
-  )
+  const action = useAction()
+
+  // switch statement handles the Component Rendering
+  switch (action) {
+    case 'rent':
+      return (
+        <Rent />
+      )
+  
+    case 'return':
+      return (
+        <Return />
+      )
+
+    default:
+      return (
+          <Text 
+            tag='h3'
+            text="Please click 'Rent a Room' or 'Return a Room' to continue!"
+            id='defaultMsg'
+          />
+      )
+  }
 }
